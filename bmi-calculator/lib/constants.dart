2a38a4b67c7b7eeabe9c18dccd5b3168af4427f3
Dulio Denis <dulio.denis@yahoo.com>
2019-07-01 18:47:10 -0400

    Updated styling on Calculate bottom button

--- bmi-calculator/lib/constants.dart
+++ bmi-calculator/lib/constants.dart
@@ -24,3 +24,6 @@ const kLabelTextStyle = TextStyle(
 );
 
 const kLabelNumberStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
+
+const kLargeButtonTextStyle =
+    TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
