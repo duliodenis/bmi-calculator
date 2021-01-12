96c566df9006c90f76aaffa26faacb5e712101e8
Dulio Denis <dulio.denis@yahoo.com>
2021-01-11 20:24:53 -0500

    Added an Xcode 12.2 / Swift 5.2 Initial App

---
+++ bmi-calculator-swift/bmi-calculator/bmi-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi-calculator
+//
+//  Created by Dulio Denis on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
