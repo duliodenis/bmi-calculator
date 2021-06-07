41a1c09b0b9016e2d9de9ad4a5fdd059f88440f4
Dulio Denis <dulio.denis@yahoo.com>
2021-06-07 19:11:22 -0400

    Update height and weight outlets when Slider values change

--- bmi-calculator-swift/bmi-calculator/bmi-calculator/Controllers/ViewController.swift
+++ bmi-calculator-swift/bmi-calculator/bmi-calculator/Controllers/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightValueChange(_ sender: UISlider) {
+        print(round(sender.value*100)/100)
+        heightLabel.text = "\(String(round(sender.value*100)/100))m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        print(Int(round(sender.value)))
+        weightLabel.text = "\(String(Int(round(sender.value))))Kgs"
+    }
+    
 }
 
