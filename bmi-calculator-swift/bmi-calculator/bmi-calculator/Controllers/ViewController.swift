1318497258d7781a73c31cda1c75213a3205c525
Dulio Denis <dulio.denis@yahoo.com>
2021-06-08 11:30:48 -0400

    Added a BMI calculate IBAction for the button

--- bmi-calculator-swift/bmi-calculator/bmi-calculator/Controllers/ViewController.swift
+++ bmi-calculator-swift/bmi-calculator/bmi-calculator/Controllers/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,14 +20,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func heightValueChange(_ sender: UISlider) {
-        print(round(sender.value*100)/100)
         heightLabel.text = "\(String(round(sender.value*100)/100))m"
     }
     
     @IBAction func weightSliderChange(_ sender: UISlider) {
-        print(Int(round(sender.value)))
         weightLabel.text = "\(String(Int(round(sender.value))))Kgs"
     }
     
+    @IBAction func calculate(_ sender: UIButton) {
+        // BMI = weight / height * height
+        let bmi = weightSlider.value / pow(heightSlider.value, 2)
+        print (bmi)
+    }
 }
 
